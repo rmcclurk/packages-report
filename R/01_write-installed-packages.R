8a713ecd0f028867fe031bbd9b33e4d00a7ffd08
McClurkin <ryamcc1@jewelry.acn>
2018-02-01 12:37:49 -0500

    Updated with results

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -16,3 +16,15 @@
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
+library(tidyverse)
+library(here)
+
+ipt <- installed.packages() %>%
+  as_tibble() %>%
+  select(Package, LibPath, Version, Priority, Built) %>%
+  write_csv(path = here("data", "installed-packages.csv"))
+
+## IRL I would might incorporate a date, but clunky for today's purpose
+## e.g., here("data", paste0(Sys.Date(),"_installed-packages.csv"))
+
+## View(ipt)
